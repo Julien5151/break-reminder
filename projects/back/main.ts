ae695c5019dded573eacf6a0f56049fc408ebd7b
Julien CLAUDEL <julienclaudel51@outlook.com>
2022-01-15 15:23:47 +0100

    Basic development setup

--- projects/back/main.ts
+++ projects/back/main.ts
@@ -1,1 +1,14 @@
-console.log('test');
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile('./dist/front/index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
