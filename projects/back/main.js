62f0ead8b67d30a87f9b10fe38d2545ba4bb6a80
Julien CLAUDEL <julienclaudel51@outlook.com>
2022-01-17 14:09:03 +0100

    Added icon

--- projects/back/main.js
+++ projects/back/main.js
@@ -1,14 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: './dist/front/assets/images/logo_final_460px.png',
   });
   win.loadFile('./dist/front/index.html');
 };
 
 app.whenReady().then(() => {
+  const appIcon = new Tray('./dist/front/assets/images/logo_final_460px.png');
   createWindow();
 
   app.on('window-all-closed', () => {
