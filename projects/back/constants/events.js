877d79cf4596a9e3cc75b439ef726ffe465fe958
Julien CLAUDEL <julienclaudel51@outlook.com>
2022-01-22 10:43:40 +0100

    Handled window lifecycle within tray

--- projects/back/constants/events.js
+++ projects/back/constants/events.js
@@ -1,5 +1,15 @@
-const WINDOW_ALL_CLOSED_EVENT = 'window-all-closed';
+const APP_WINDOW_ALL_CLOSED_EVENT = 'window-all-closed';
+const APP_QUIT = 'quit';
+const APP_BEFORE_QUIT = 'before-quit';
+const WIN_CLOSE = 'close';
+const WIN_MINIMIZE = 'minimize';
+const WIN_MAXIMIZE = 'maximize';
 
 module.exports = {
-  WINDOW_ALL_CLOSED_EVENT,
+  APP_WINDOW_ALL_CLOSED_EVENT,
+  APP_QUIT,
+  APP_BEFORE_QUIT,
+  WIN_CLOSE,
+  WIN_MINIMIZE,
+  WIN_MAXIMIZE,
 };
--- projects/back/main.js
+++ projects/back/main.js
@@ -1,21 +1,36 @@
-const { app, BrowserWindow, Tray, nativeImage } = require('electron');
-const { WINDOW_ALL_CLOSED_EVENT } = require('./constants/events');
-const { MACOS } = require('./constants/os');
+const { app, BrowserWindow, Tray, nativeImage, Menu } = require('electron');
 const { LOGO_64_PX_PATH, SPA_INDEX_HTML_PATH } = require('./constants/paths');
 
 app.whenReady().then(() => {
+  // Create icon
   const icon = nativeImage.createFromPath(LOGO_64_PX_PATH);
-  const tray = new Tray(icon);
-
-  const win = new BrowserWindow({
+  // Instanciate window
+  const window = new BrowserWindow({
     width: 800,
     height: 600,
     icon: icon,
   });
   // Remove menu window
-  win.loadFile(SPA_INDEX_HTML_PATH);
-
-  app.on(WINDOW_ALL_CLOSED_EVENT, () => {
-    if (process.platform !== MACOS) app.quit();
-  });
+  window.loadFile(SPA_INDEX_HTML_PATH);
+  // Instanciate tray
+  const tray = new Tray(icon);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open settings',
+      click: () => {
+        window.show();
+      },
+    },
+    {
+      label: 'Close',
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
+  // Workaround to make tray menu items work after first blur,
+  // must be added asynchronously
+  setTimeout(() => {
+    tray.setContextMenu(contextMenu);
+  }, 0);
 });
